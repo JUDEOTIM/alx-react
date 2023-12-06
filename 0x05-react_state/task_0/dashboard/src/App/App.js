280729a67c4424dbf1dd8bf80d37b29b8edf0c6a
JUDEOTIM <otimjudeoj@gmail.com>
2023-12-06 13:02:40 +0300

    Adding a local state for notifications

---
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
---
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { shallow } from "enzyme";
+import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
+import BodySection from "./BodySection";
+import { StyleSheetTestUtils } from "aphrodite";
